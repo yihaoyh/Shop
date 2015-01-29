2a6080d1fdf5ac79a1d34a76bc1c0fe971f39483
yihao <yhchinabest@gmail.com>
2015-01-29 22:42:32 +0800

    upload modify

--- api.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('APP_DEBUG',TRUE); //开启调试模式
-define('APP_NAME', './shop');
-define('APP_PATH', 'Api/');
-ini_set('date.timezone','Asia/Shanghai');
-require("ThinkPHP/ThinkPHP.php");
-
-
-?>
